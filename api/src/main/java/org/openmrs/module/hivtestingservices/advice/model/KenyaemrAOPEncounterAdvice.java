7dfe8e56659a6f189502a032ac6f1c4a5b68ca76
ojwanganto <ojwangwachiaje@gmail.com>
2018-04-25 15:28:32 +0300

    defined model for aop encounter form

--- api/src/main/java/org/openmrs/module/hivtestingservices/advice/model/KenyaemrAOPEncounterAdvice.java
+++ api/src/main/java/org/openmrs/module/hivtestingservices/advice/model/KenyaemrAOPEncounterAdvice.java
@@ -1,4 +1,67 @@
 package org.openmrs.module.hivtestingservices.advice.model;
 
-public class KenyaemrAOPEncounterAdvice {
+import org.openmrs.BaseOpenmrsData;
+
+import java.util.Date;
+
+public class KenyaemrAOPEncounterAdvice extends BaseOpenmrsData {
+
+    private Integer id;
+    private String encounterUUID;
+    private String formUUID;
+    private String targetModule;
+    private Integer status;
+    private Date dateCreated;
+
+    @Override
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEncounterUUID() {
+        return encounterUUID;
+    }
+
+    public void setEncounterUUID(String encounterUUID) {
+        this.encounterUUID = encounterUUID;
+    }
+
+    public String getFormUUID() {
+        return formUUID;
+    }
+
+    public void setFormUUID(String formUUID) {
+        this.formUUID = formUUID;
+    }
+
+    public String getTargetModule() {
+        return targetModule;
+    }
+
+    public void setTargetModule(String targetModule) {
+        this.targetModule = targetModule;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    @Override
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    @Override
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
 }
