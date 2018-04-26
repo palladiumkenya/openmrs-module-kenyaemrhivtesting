f5537b4b9eaa6fa6dcf0be99b00d158a40c2886f
ojwanganto <ojwangwachiaje@gmail.com>
2018-04-26 17:17:30 +0300

    successfully inserted contacts from contact listing form to patientContact model

---
+++ api/src/main/java/org/openmrs/module/hivtestingservices/advice/model/FamilyHistoryFormContactListingProcessor.java
@@ -0,0 +1,176 @@
+package org.openmrs.module.hivtestingservices.advice.model;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.joda.time.DateTimeZone;
+import org.joda.time.LocalDate;
+import org.joda.time.Period;
+import org.openmrs.Concept;
+import org.openmrs.Encounter;
+import org.openmrs.Obs;
+import org.openmrs.api.ConceptService;
+import org.openmrs.api.EncounterService;
+import org.openmrs.api.ObsService;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.hivtestingservices.api.HTSService;
+import org.openmrs.module.hivtestingservices.api.PatientContact;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class FamilyHistoryFormContactListingProcessor {
+
+    protected static final Log log = LogFactory.getLog(FamilyHistoryFormContactListingProcessor.class);
+
+    HTSService htsService = Context.getService(HTSService.class);
+    EncounterService encounterService = Context.getEncounterService();
+    ObsService obsService = Context.getObsService();
+    ConceptService conceptService = Context.getConceptService();
+
+    public void processAOPEncounterEntries() {
+
+        String familyHistoryGroupingConcept = "160593AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+
+        // Fetch aop entries
+        List<AOPEncounterEntry> aopEntries = htsService.getAopEncounterEntryList();
+        for(AOPEncounterEntry aopEntry : aopEntries) {
+            Encounter enc = encounterService.getEncounterByUuid(aopEntry.getEncounterUUID());
+
+            // construct object for each contact and process them
+                List<Obs> obs = obsService.getObservations(
+                        Arrays.asList(Context.getPersonService().getPerson(enc.getPatient().getPersonId())),
+                        Arrays.asList(enc),
+                        Arrays.asList(conceptService.getConceptByUuid(familyHistoryGroupingConcept)),
+                        null,
+                        null,
+                        null,
+                        Arrays.asList("obsId"),
+                        null,
+                        null,
+                        null,
+                        null,
+                        false
+                );
+                for(Obs o: obs) {
+                    if (extractFamilyAndPartnerTestingRows(o.getGroupMembers()) != null) {
+                        PatientContact contact = extractFamilyAndPartnerTestingRows(o.getGroupMembers());
+                        contact.setObsGroupId(o);
+                        contact.setPatientRelatedTo(Context.getPatientService().getPatient(o.getPersonId()));
+                        htsService.savePatientContact(contact);
+                    }
+                }
+            // update processed aop entry
+            aopEntry.setStatus(1);
+                htsService.saveAopEncounterEntry(aopEntry);
+        }
+    }
+
+    private PatientContact fillContactName(String fullName) {
+        if(fullName != null && !fullName.equals("")) {
+            String[] nameParts = StringUtils.split(fullName);
+            PatientContact contact = new PatientContact();
+            if(nameParts.length == 1) {
+                contact.setFirstName(fullName);
+            } else if(nameParts.length == 2) {
+                contact.setFirstName(nameParts[0]);
+                contact.setLastName(nameParts[1]);
+            } else {
+                contact.setFirstName(nameParts[0]);
+                contact.setLastName(nameParts[1]);
+                contact.setMiddleName(nameParts[2]);
+            }
+            return contact;
+        }
+        return null;
+    }
+
+    PatientContact extractFamilyAndPartnerTestingRows (Set<Obs> obsList) {
+
+        Integer contactConcept = 160750;
+        Integer	ageConcept = 160617;
+        Integer relationshipConcept = 1560;
+        Integer baselineHivStatusConcept =1169;
+        Integer nextTestingDateConcept = 164400;
+        Integer ageUnitConcept = 1732;
+
+        Integer relType = null;
+        Integer age = 0;
+        String baselineStatus = null;
+        Date nextTestDate = null;
+        String contactName = null;
+        Integer ageUnit = null;
+
+
+        for(Obs obs:obsList) {
+
+            if (obs.getConcept().getConceptId().equals(contactConcept) ) {
+                contactName = obs.getValueText();
+            } else if (obs.getConcept().getConceptId().equals(ageConcept )) { // get age
+                age = obs.getValueNumeric().intValue();
+            } else if (obs.getConcept().getConceptId().equals(baselineHivStatusConcept) ) {
+                baselineStatus = hivStatusConverter(obs.getValueCoded());
+            } else if (obs.getConcept().getConceptId().equals(nextTestingDateConcept )) {
+                nextTestDate = obs.getValueDate();
+            } else if (obs.getConcept().getConceptId().equals(relationshipConcept) ) {
+                relType = relationshipConverter(obs.getValueCoded());
+            } else if (obs.getConcept().getConceptId().equals(ageUnitConcept) ) {
+                ageUnit = obs.getValueCoded().getConceptId();
+            }
+        }
+        if(contactName != null) {
+            PatientContact contact = fillContactName(contactName);
+            contact.setRelationType(relType);
+            contact.setBaselineHivStatus(baselineStatus);
+            contact.setAppointmentDate(nextTestDate);
+            contact.setBirthDate(calculateDobFromAge(age, ageUnit));
+            contact.setSex("Undefined");
+
+            return contact;
+        }
+        return null;
+
+    }
+
+    private Date calculateDobFromAge(int age, Integer unit) {
+        LocalDate now = LocalDate.now(DateTimeZone.forID("Africa/Nairobi"));
+        Period agePeriod;
+        if(unit == 1734) { // age provided in years
+            agePeriod = new Period(age, 0, 0, 0, 0, 0, 0, 0);
+        } else { // age provided in months
+            agePeriod = new Period(0, age, 0, 0, 0, 0, 0, 0);
+        }
+
+        //(int years, int months, int weeks, int days, int hours, int minutes, int seconds, int millis)
+        LocalDate jodaDob = now.minus(agePeriod);
+        return jodaDob.toDateTimeAtStartOfDay().toDate();
+
+    }
+
+    Integer relationshipConverter (Concept key) {
+        Map<Concept, Integer> relationshipList = new HashMap<Concept, Integer>();
+        relationshipList.put(conceptService.getConcept(970), 3); // parent
+        relationshipList.put(conceptService.getConcept(971), 3); //parent
+        relationshipList.put(conceptService.getConcept(972), 2); //sibling
+        relationshipList.put(conceptService.getConcept(1528), 3); //child
+        relationshipList.put(conceptService.getConcept(5617), 6); // spouse
+        relationshipList.put(conceptService.getConcept(163565), 7); // partner
+        relationshipList.put(conceptService.getConcept(162221), 8); // co-wife
+
+        return relationshipList.get(key);
+    }
+
+    String hivStatusConverter (Concept key) {
+        Map<Concept, String> hivStatusList = new HashMap<Concept, String>();
+        hivStatusList.put(conceptService.getConcept(703), "Positive");
+        hivStatusList.put(conceptService.getConcept(664), "Negative");
+        hivStatusList.put(conceptService.getConcept(1405), "Exposed");
+        hivStatusList.put(conceptService.getConcept(1067), "Unknown");
+        return hivStatusList.get(key);
+    }
+
+}
---
+++ api/src/main/java/org/openmrs/module/hivtestingservices/task/ContactListingFormProcessorTask.java
@@ -0,0 +1,51 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.hivtestingservices.task;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.hibernate.Transaction;
+import org.hibernate.jdbc.Work;
+import org.openmrs.api.context.Context;
+import org.openmrs.api.db.hibernate.DbSessionFactory;
+import org.openmrs.module.hivtestingservices.advice.model.FamilyHistoryFormContactListingProcessor;
+import org.openmrs.scheduler.tasks.AbstractTask;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Periodically refreshes ETL tables
+ */
+public class ContactListingFormProcessorTask extends AbstractTask {
+
+	private Log log = LogFactory.getLog(getClass());
+
+	/**
+	 * @see AbstractTask#execute()
+	 */
+	public void execute() {
+		Context.openSession();
+		try {
+
+			if (!Context.isAuthenticated()) {
+				authenticate();
+			}
+			FamilyHistoryFormContactListingProcessor processor = new FamilyHistoryFormContactListingProcessor();
+			processor.processAOPEncounterEntries();
+
+		}
+		catch (Exception e) {
+			throw new IllegalArgumentException("Unable to execute query", e);
+		}
+	}
+	
+}
