96de785beba6acccad7a3cf136c3b50bb31d7356
ojwanganto <ojwangwachiaje@gmail.com>
2021-04-06 14:22:40 +0300

    fix for kemr reference in afyastat

--- api/src/main/java/org/openmrs/module/hivtestingservices/handler/JsonContactListQueueDataHandler.java
+++ api/src/main/java/org/openmrs/module/hivtestingservices/handler/JsonContactListQueueDataHandler.java
@@ -17,6 +17,7 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openmrs.Patient;
+import org.openmrs.Person;
 import org.openmrs.annotation.Handler;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.hivtestingservices.api.HTSService;
@@ -109,10 +110,13 @@ public class JsonContactListQueueDataHandler implements QueueDataHandler {
         String physicalAddress = JsonUtils.readAsString(payload, "$['physical_address']");
 
         Integer patientRelatedTo = null;
-        String indexKemrUuid = JsonUtils.readAsString(payload, "$['parent']['kemr_uuid']");
-        patientRelatedTo = org.apache.commons.lang3.StringUtils.isNotBlank(indexKemrUuid) ? getPatientRelatedToContact(indexKemrUuid) : getPatientRelatedToContact(JsonUtils.readAsString(payload, "$['parent']['_id']"));
+        String kemrRef = JsonUtils.readAsString(payload, "$['parent']['kemr_uuid']");
+        patientRelatedTo = org.apache.commons.lang3.StringUtils.isNotBlank(kemrRef) ? getPatientRelatedToContact(kemrRef) : getPatientRelatedToContact(JsonUtils.readAsString(payload, "$['parent']['_id']"));
         String uuid = JsonUtils.readAsString(payload, "$['_id']");
         Boolean voided= false;
+
+        System.out.println(", kemr_uuid: " + kemrRef + ", patient related to: " + patientRelatedTo );
+        log.info( ", kemr_uuid: " + kemrRef + ", patient related to: " + patientRelatedTo );
 
         if (org.apache.commons.lang3.StringUtils.isNotBlank(baselineStatus)) {
 
@@ -132,11 +136,15 @@ public class JsonContactListQueueDataHandler implements QueueDataHandler {
         unsavedPatientContact.setLastName(familyName);
         unsavedPatientContact.setRelationType(relType);
         unsavedPatientContact.setBaselineHivStatus(baselineStatus);
-        unsavedPatientContact.setAppointmentDate(nextTestDate);
+        if (nextTestDate != null) {
+            unsavedPatientContact.setAppointmentDate(nextTestDate);
+        }
         unsavedPatientContact.setBirthDate(birthDate);
         unsavedPatientContact.setSex(sex);
         unsavedPatientContact.setPhoneContact(phoneNumber);
-        unsavedPatientContact.setMaritalStatus(maritalStatus);
+        if (maritalStatus != null) {
+            unsavedPatientContact.setMaritalStatus(maritalStatus);
+        }
         unsavedPatientContact.setLivingWithPatient(livingWithPatient);
         unsavedPatientContact.setPnsApproach(pnsApproach);
         unsavedPatientContact.setContactListingDeclineReason("CHT");// using this to identify contact pushed from CHT
@@ -179,6 +187,17 @@ public class JsonContactListQueueDataHandler implements QueueDataHandler {
             Patient p = Context.getPatientService().getPatientByUuid(regData.getAssignedUuid());
             if (p !=null){
                  patientId= p.getPatientId();
+                 System.out.println("Patient not null");
+            }
+        }
+
+        if (patientId == null) {
+            System.out.println("Patient was null. Looking to get a person");
+            // check to see if the uuid is for patient
+            Person person = Context.getPersonService().getPersonByUuid(uuid);
+            if (person != null) {
+                patientId = person.getPersonId();
+                System.out.println("");
             }
         }
         return patientId;
