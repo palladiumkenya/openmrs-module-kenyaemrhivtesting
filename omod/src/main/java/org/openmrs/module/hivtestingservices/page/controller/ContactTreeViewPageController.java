e2705de9a8d194ed61673bdd83481c2646e55770
ojwanganto <ojwangwachiaje@gmail.com>
2021-07-02 10:33:45 +0300

    added app page for pns tree view

---
+++ omod/src/main/java/org/openmrs/module/hivtestingservices/page/controller/ContactTreeViewPageController.java
@@ -0,0 +1,13 @@
+package org.openmrs.module.hivtestingservices.page.controller;
+
+import org.openmrs.module.kenyaui.annotation.AppPage;
+
+@AppPage("kenyaemr.hivtesting")
+public class ContactTreeViewPageController {
+    public void controller() {
+
+    }
+}
+
+
+
