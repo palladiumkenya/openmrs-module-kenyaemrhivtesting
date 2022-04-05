26eca9b4e65bd6c13ebca965784de56b19bec433
ojwanganto <ojwangwachiaje@gmail.com>
2022-04-05 10:36:40 +0300

    skip registration of patient contacts who miss the mandatory first/last name and/or dob

--- api/src/main/java/org/openmrs/module/hivtestingservices/api/PatientContact.java
+++ api/src/main/java/org/openmrs/module/hivtestingservices/api/PatientContact.java
@@ -7,6 +7,7 @@ import org.openmrs.Patient;
 import org.openmrs.User;
 
 import java.util.Date;
+import java.util.Objects;
 import java.util.UUID;
 
 public class PatientContact extends BaseOpenmrsData {
@@ -300,6 +301,20 @@ public class PatientContact extends BaseOpenmrsData {
     @Override
     public void setVoidReason(String voidReason) {
         super.setVoidReason(voidReason);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        PatientContact that = (PatientContact) o;
+        return id.equals(that.id) && uuid.equals(that.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, uuid);
     }
 }
 
