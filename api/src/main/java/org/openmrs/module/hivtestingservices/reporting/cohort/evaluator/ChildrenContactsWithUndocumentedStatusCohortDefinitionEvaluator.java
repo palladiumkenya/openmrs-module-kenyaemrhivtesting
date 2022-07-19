7eb67760f648fd5de4b9f25ef03578d423821c6b
Stephen Chege <njorocs@gmail.com>
2022-07-19 17:53:32 +0300

    Aligned contact linelists with CAR indicators

--- api/src/main/java/org/openmrs/module/hivtestingservices/reporting/cohort/evaluator/ChildrenContactsWithUndocumentedStatusCohortDefinitionEvaluator.java
+++ api/src/main/java/org/openmrs/module/hivtestingservices/reporting/cohort/evaluator/ChildrenContactsWithUndocumentedStatusCohortDefinitionEvaluator.java
@@ -7,7 +7,6 @@ import org.openmrs.module.hivtestingservices.query.patientContact.PatientContact
 import org.openmrs.module.hivtestingservices.query.patientContact.definition.PatientContactQuery;
 import org.openmrs.module.hivtestingservices.query.patientContact.evaluator.PatientContactQueryEvaluator;
 import org.openmrs.module.hivtestingservices.reporting.cohort.definition.ChildrenContactsWithUndocumentedStatusCohortDefinition;
-import org.openmrs.module.hivtestingservices.reporting.cohort.definition.SNSContactsWithUndocumentedStatusCohortDefinition;
 import org.openmrs.module.reporting.common.ObjectUtil;
 import org.openmrs.module.reporting.evaluation.EvaluationContext;
 import org.openmrs.module.reporting.evaluation.EvaluationException;
@@ -33,16 +32,36 @@ public class ChildrenContactsWithUndocumentedStatusCohortDefinitionEvaluator imp
 		context = ObjectUtil.nvl(context, new EvaluationContext());
 		PatientContactQueryResult queryResult = new PatientContactQueryResult(definition, context);
 
-		String qry = "select c.id from (select pc.id,pc.patient_id from kenyaemr_hiv_testing_patient_contact pc\n" +
-				"        left join patient p on p.patient_id = pc.patient_related_to and p.voided = 0\n" +
-				"        left join (select ht.patient_id, mid(max(concat(ht.final_test_result)),11) as hiv_status\n" +
-				"                   from kenyaemr_etl.etl_hts_test ht group by ht.patient_id\n" +
-				"                   having hiv_status in ('Negative','Positive'))ht on ht.patient_id = pc.patient_id\n" +
-				"where (pc.baseline_hiv_status not in ('Positive','Negative') or pc.baseline_hiv_status is null) and pc.relationship_type =1528 and\n" +
-				"date(pc.date_created) <= date(:endDate) and pc.voided = 0 and ht.patient_id is null)c\n" +
-				"left join (select e.patient_id from kenyaemr_etl.etl_hei_enrollment e inner join kenyaemr_etl.etl_patient_demographics d on d.patient_id = e.patient_id\n" +
-				"and timestampdiff(WEEK,d.dob,date(:endDate)) between 6 and 96) h on c.patient_id = h.patient_id\n" +
-				"where h.patient_id is null;";
+		String qry = "select c.id\n" +
+				"from (select pc.id,\n" +
+				"             pc.patient_id,\n" +
+				"             pc.patient_related_to,\n" +
+				"             h.patient_id  as hei,\n" +
+				"             pc.baseline_hiv_status,\n" +
+				"             pc.relationship_type,\n" +
+				"             pc.date_created,\n" +
+				"             pc.voided,\n" +
+				"             ht.patient_id as tested_contact\n" +
+				"      from openmrs.kenyaemr_hiv_testing_patient_contact pc\n" +
+				"               inner join openmrs.patient p on p.patient_id = pc.patient_related_to and p.voided = 0\n" +
+				"               left join (select ht.patient_id,\n" +
+				"                                 mid(max(concat(date(ht.visit_date), ht.final_test_result)), 11) as hiv_status\n" +
+				"                          from kenyaemr_etl.etl_hts_test ht\n" +
+				"                          group by ht.patient_id\n" +
+				"                          having hiv_status in ('Negative', 'Positive')) ht on ht.patient_id = pc.patient_id\n" +
+				"               left join (select e.patient_id, e.hiv_status_at_exit\n" +
+				"                          from kenyaemr_etl.etl_hei_enrollment e\n" +
+				"                                   inner join kenyaemr_etl.etl_patient_demographics d on d.patient_id = e.patient_id\n" +
+				"                              and timestampdiff(WEEK, d.dob, date(:endDate)) between 6 and 96) h\n" +
+				"                         on pc.patient_id = h.patient_id\n" +
+				"     ) c\n" +
+				"where\n" +
+				"  date(c.date_created) <= date(:endDate)\n" +
+				"  and c.hei is null\n" +
+				"  and (c.baseline_hiv_status is null or c.baseline_hiv_status in ('Unknown','1067'))\n" +
+				"  and c.relationship_type = 1528\n" +
+				"  and c.voided = 0\n" +
+				"  and c.tested_contact is null;";
 
 		SqlQueryBuilder builder = new SqlQueryBuilder();
 		builder.append(qry);
