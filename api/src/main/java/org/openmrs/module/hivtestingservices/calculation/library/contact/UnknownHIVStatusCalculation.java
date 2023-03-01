1f5eeda22d1061d4ab25142a969d7d14696dd5ff
njorocs <njorocs@gmail.com>
2023-03-01 20:31:53 +0300

    Revised calculation for flagging index clients with contacts whose HIV status is not to extend search in HTS

--- api/src/main/java/org/openmrs/module/hivtestingservices/calculation/library/contact/UnknownHIVStatusCalculation.java
+++ api/src/main/java/org/openmrs/module/hivtestingservices/calculation/library/contact/UnknownHIVStatusCalculation.java
@@ -11,6 +11,11 @@ package org.openmrs.module.hivtestingservices.calculation.library.contact;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openmrs.Concept;
+import org.openmrs.Encounter;
+import org.openmrs.Patient;
+import org.openmrs.api.ConceptService;
+import org.openmrs.api.EncounterService;
 import org.openmrs.api.context.Context;
 import org.openmrs.calculation.patient.PatientCalculationContext;
 import org.openmrs.calculation.result.CalculationResultMap;
@@ -19,7 +24,10 @@ import org.openmrs.module.hivtestingservices.api.PatientContact;
 import org.openmrs.module.kenyacore.calculation.AbstractPatientCalculation;
 import org.openmrs.module.kenyacore.calculation.BooleanResult;
 import org.openmrs.module.kenyacore.calculation.PatientFlagCalculation;
+import org.openmrs.module.kenyaemr.util.EmrUtils;
+import org.openmrs.module.kenyaemr.util.HtsConstants;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -50,19 +58,52 @@ public class UnknownHIVStatusCalculation extends AbstractPatientCalculation impl
     public CalculationResultMap evaluate(Collection<Integer> cohort, Map<String, Object> parameterValues,
                                          PatientCalculationContext context) {
 
+        EncounterService encounterService = Context.getEncounterService();
+
         CalculationResultMap ret = new CalculationResultMap();
 
         for (int ptId : cohort) {
             boolean eligible = false;
-
             List<PatientContact> patientContacts;
             patientContacts = htsService.getPatientContactByPatient(Context.getPatientService().getPatient(ptId));
 
             if (!patientContacts.isEmpty()) {
+
+                ConceptService cs = Context.getConceptService();
+                Concept htsFinalTestQuestion = cs.getConcept(HtsConstants.HTS_FINAL_TEST_CONCEPT_ID);
+                Concept htsPositiveResult = cs.getConcept(HtsConstants.HTS_POSITIVE_RESULT_CONCEPT_ID);
+                Concept htsNegativeResult = cs.getConcept(HtsConstants.HTS_NEGATIVE_RESULT_CONCEPT_ID);
+
                 for (PatientContact pc : patientContacts) {
                     if (pc.getVoided().equals(false)) {
 
-                        if (pc.getBaselineHivStatus() == null || !pc.getBaselineHivStatus().equalsIgnoreCase("Positive") && !pc.getBaselineHivStatus().equalsIgnoreCase("Negative")) {
+                        //Checking for HTS encounters for valid test results (Positive or Negative)
+                        Patient patient = pc.getPatient();
+
+                        List<Encounter> htsEncounters = encounterService.getEncounters(patient, null, null, null, Arrays.asList(HtsConstants.htsInitialForm, HtsConstants.htsRetestForm), null, null, null, null, false);
+
+                        Encounter lastHtsInitialEnc = EmrUtils.lastEncounter(patient, HtsConstants.htsEncType, HtsConstants.htsInitialForm);
+                        Encounter lastHtsRetestEnc = EmrUtils.lastEncounter(patient, HtsConstants.htsEncType, HtsConstants.htsRetestForm);
+
+                        Encounter lastHtsEnc = null;
+
+                        if (lastHtsInitialEnc != null && lastHtsRetestEnc == null) {
+                            lastHtsEnc = lastHtsInitialEnc;
+                        } else if (lastHtsInitialEnc == null && lastHtsRetestEnc != null) {
+                            lastHtsEnc = lastHtsRetestEnc;
+                        } else if (lastHtsInitialEnc != null && lastHtsRetestEnc != null) {
+                            if (lastHtsInitialEnc.getEncounterDatetime().after(lastHtsRetestEnc.getEncounterDatetime())) {
+                                lastHtsEnc = lastHtsInitialEnc;
+                            } else {
+                                lastHtsEnc = lastHtsRetestEnc;
+                            }
+                        }
+
+                        boolean patientHasNegativeTestResult = htsEncounters.size() > 0 && lastHtsEnc != null ? EmrUtils.encounterThatPassCodedAnswer(lastHtsEnc, htsFinalTestQuestion, htsNegativeResult) : false;
+                        boolean patientHasPositiveTestResult = htsEncounters.size() > 0 && lastHtsEnc != null ? EmrUtils.encounterThatPassCodedAnswer(lastHtsEnc, htsFinalTestQuestion, htsPositiveResult) : false;
+
+                        //Checking for Unknown status in Patient contact and HTS
+                        if (pc.getBaselineHivStatus() == null || (!pc.getBaselineHivStatus().equalsIgnoreCase("Positive") && !pc.getBaselineHivStatus().equalsIgnoreCase("Negative") && !patientHasNegativeTestResult && !patientHasPositiveTestResult)) {
                             eligible = true;
                             break;
                         }
